7fd4429268cf666189592eec379997568ce23c49
Shubhra Singhal <shubhrasinghal2015@gmail.com>
2021-12-03 10:29:47 +0530

    changed schema

--- testBeta1.sql
+++ testBeta1.sql
@@ -1,2 +1,2 @@
-insert into "NEW"."dimcustomer"  (primaryemail, registrationdate, name) 
+insert into "STAGE"."dimcustomer"  (primaryemail, registrationdate, name) 
 values ('sdf.com', '2055455', 'shubhra')
