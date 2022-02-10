eef6e339785752e9d9396f885eb76f37c89551a2
coderss21 <shubhrasinghal2015@gmail.com>
2022-02-10 22:31:32 +0530

    removed

--- test1.sql
+++
@@ -1,2 +0,0 @@
-insert into "SNOWTEST"."dimcustomer"  (primaryemail, registrationdate, name) 
-values ('asdf', 'Asf', 'test')
--- testBeta1.sql
+++
@@ -1,2 +0,0 @@
-insert into "SHUBHRA"."dimcustomer"  (primaryemail, registrationdate, name) 
-values ('sdf.com', '2055455', 'shubhra')
