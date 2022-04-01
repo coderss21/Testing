b35329bcaee310a84bbd6f0735f0992427ff191d
Shubhra Singhal <shubhrasinghal2015@gmail.com>
2022-04-01 11:52:12 +0530

    created file to print long logs in python

---
+++ longslogs.py
@@ -0,0 +1,13 @@
+import time
+print("Hello World")
+i = 1
+while i <= 400:
+    print("Welcome ", i,"times")
+    time.sleep(60);
+    i += 1
+
+i=10
+while i<=1000 :
+    print("Hello I am ",i,"times")
+    time.sleep(3)
+    i+=100
